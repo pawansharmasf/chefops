a7a9f38a5e8cefa40e2a9f72f2c41dab501d7481
pawansharmasf <erapawan@gmail.com>
2017-03-21 15:58:05 +0530

    Update mysql_ubuntu.rb

--- softwares/recipes/mysql_ubuntu.rb
+++ softwares/recipes/mysql_ubuntu.rb
@@ -2,8 +2,3 @@ execute 'install_mysql' do
   command "apt-get install mysql-server mysql-client -y"
   action :run
 end
-
-execute 'run_mysql' do
-  command "service mysql start"
-  action :run
-end
